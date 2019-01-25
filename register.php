01e10a7984dd2ce4573f3537c54e450368d898b8
MikeG212 <michaelcgee93@gmail.com>
2019-01-24 18:11:39 -0800

    Create sign up form

--- register.php
+++ register.php
@@ -18,6 +18,40 @@
 
             <button type="submit" name="loginButton">LOG IN</button>
         </form>
+
+        <form id="registerForm" action="register.php" method="POST">
+            <h2>Create your free account<h2>
+           <p>
+                <label for="signUpUsername">Username</label>
+           <input id="signUpUsername" name="signUpUsername" type="text" placeholder="e.g. HomerJSimpson" required>
+           </p>
+            <p>
+                <label for="firstName">First Name</label>
+                <input id="firstName" name="firstName" type="text" placeholder="Homer" required>
+           </p>
+           <p>
+                <label for="lastName">Last Name</label>
+                <input id="lastName" name="lastName" type="text" placeholder="Simpson" required>
+           </p>
+           <p>
+                <label for="email">Email</label>
+                <input id="email" name="email" type="email" placeholder="homer@gmail.com" required>
+           </p>
+           <p>
+                <label for="email2">Confirm Email</label>
+                <input id="email2" name="email2" type="email" placeholder="homer@gmail.com" required>
+           </p>
+           <p>
+                <label for="signUpPassword">Password</label>
+                <input id="signUpPassword" name="signUpPassword" type="password" required> 
+            </p>
+            <p>
+                <label for="signUpPassword2">Confirm Password</label>
+                <input id="signUpPassword2" name="signUpPassword2" type="password" required> 
+            </p>
+
+            <button type="submit" name="signUpBottom">Sign Up</button>
+        </form>
     <div>
 </body>
 </html>
