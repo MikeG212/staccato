58147e30559801e105e9ac22ba5bd42a527118b7
MikeG212 <michaelcgee93@gmail.com>
2019-01-25 15:25:25 -0800

    Create password validation methods

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -58,6 +58,22 @@
         }
         
         private function validatePasswords($pw, $pw2) {
+            if($pw != $pw2) {
+                array_push($this->errorArray, "Your passwords don't match");
+                return;
+            }
+
+            if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+               array_push($this->errorArray, "Your passwords can only contain numbers and letters");
+               return; 
+            }
+
+            if(strlen($pw) > 30 || strlen($pw) < 5) {
+                array_push($this->errorArray, "Your password must be between 5 and 30 characters");
+                return;
+            }
+
+
 
         }
 
