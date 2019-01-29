0527f39226defb760e6cc0ff04d0cf678bf233ed
MikeG212 <michaelcgee93@gmail.com>
2019-01-29 12:30:11 -0800

    Create session to save logged in users

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,6 @@
 <?php
   ob_start();
+  session_start();
 
   $timezone = date_default_timezone_set("America/Los_Angeles");
 
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+include("includes/config.php");
+    if(isset($_SESSION['userLoggedIn'])) {
+        $userLoggedIn = $_SESSION['userLoggedIn'];
+    }
+    else {
+        header("Location: register.php");
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
