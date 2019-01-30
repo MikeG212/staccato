358ae0b6601049a8e7aa25394b6dec5bfbba266b
MikeG212 <michaelcgee93@gmail.com>
2019-01-29 18:14:30 -0800

    Add right content

--- register.php
+++ register.php
@@ -25,6 +25,27 @@
     <script src="assets/js/register.js"></script>
 </head>
 <body>
+    <?php
+        if (isset($_POST['registerButton'])) {
+            echo '<script>
+                    $(document).ready(function() {
+                        $("#registerForm").hide();
+                        $("#loginForm").show(); 
+                    });
+                </script>';
+            
+        }
+        else {
+            echo '<script>
+                    $(document).ready(function() {
+                        $("#registerForm").show();
+                        $("#loginForm").hide(); 
+                    });
+                </script>';
+        }
+    ?>
+    
+
     <div id="background">
 
         <div id="loginContainer">
@@ -36,7 +57,7 @@
                     <p>
                         <?php echo $account->getError(Constants::$loginFailed); ?>
                         <label for="loginUsername">Username</label>
-                        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. HomerSimpson" required>
+                        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. HomerJSimpson" required>
                     </p>
                     <p>
                         <label for="loginPassword">Password</label>
@@ -104,6 +125,16 @@
                 </form>
 
             </div>
+
+            <div id="loginText">
+                <h1>Get great music, right now</h1>
+                <h2>Listen to loads of songs for free</h2>
+
+                <ul>
+                    <li>Discover music you'll fall in love with</li>
+                    <li>Create your own playlists</li>
+                    <li>Keep up to date with hot artists</li>
+                </ul>
 
         </div>
 
