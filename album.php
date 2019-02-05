9caae98ce7ab5f91db00bc72a671eb2c7dfee552
MikeG212 <michaelcgee93@gmail.com>
2019-02-05 14:45:22 -0800

    Add unstylized album artwork to header

--- album.php
+++ album.php
@@ -10,10 +10,17 @@ else {
 $albumQuery = mysqli_query($con, "SELECT * FROM Albums WHERE id='$albumId'");
 $album = new Album($con, $albumId);
 $artist = $album->getArtist();
-
-echo $album->getTitle() . "<br>";
-echo $artist->getName();
 ?>
 
+<div class="entityInfo">
+    <div class="leftSection">
+        <img src="<?php echo $album->getArtwork(); ?>">
+    </div>
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <span>By <?php echo $artist->getName(); ?></span>
+    </div>
 
+
+</div>
 <?php include("includes/footer.php"); ?>
