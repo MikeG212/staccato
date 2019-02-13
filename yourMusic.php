f08b6a617bb9107da3e79d1b4ee07b0c06a68ab0
MikeG212 <michaelcgee93@gmail.com>
2019-02-13 13:26:25 -0800

    Create and style playlist link

--- yourMusic.php
+++ yourMusic.php
@@ -20,12 +20,15 @@ include("includes/includedFiles.php");
 
             while($row = mysqli_fetch_array($playlistsQuery)) {
 
-                echo "<div class='gridViewItem'>
+                $playlist = new Playlist($con, $row);
+
+                echo "<div class='gridViewItem' role='link' tabindex='0'
+                onclick='openPage(\"playlist.php?id=" . $playlist->getId() . "\")'>
                         <div class='playlistImage'>
                             <img src='assets/images/icons/playlist.png'>
                         </div>
                         <div class='gridViewInfo'>"
-                            . $row['name'] .
+                            . $playlist->getName() .
                         "</div>
                     </div>"; 
 
