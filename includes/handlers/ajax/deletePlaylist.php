cf93dcee75b85c2c9ba6b2af51818f1f7e59c84f
MikeG212 <michaelcgee93@gmail.com>
2019-02-13 14:27:20 -0800

    Create and style options menu

--- includes/handlers/ajax/deletePlaylist.php
+++ includes/handlers/ajax/deletePlaylist.php
@@ -1,8 +1,8 @@
 <?php
 include("../../config.php");
 
-if(isset($_GET['playlistId '])) {
-    $playlistId = $_GET['playlistId'];
+if(isset($_POST['playlistId '])) {
+    $playlistId = $_POST['playlistId'];
 
     $playlistQuery = mysqli_query($con, "DELETE FROM playlists WHERE id='playlistId'");
     $songsQuery = mysqli_query($con, "DELETE FROM playlistSongs WHERE playlistId='playlistId'");
