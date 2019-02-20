061a6f548e21e509491d766f02a9870643b88005
MikeG212 <michaelcgee93@gmail.com>
2019-02-20 12:14:47 -0800

    Create functioning logout button

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,12 @@ $(document).on("change", "select.playlist", function() {
 
 });
 
+function logout() {
+    $.post("includes/handlers/ajax/logout.php", function() {
+        location.reload();
+    });
+}
+
 function openPage(url) {
     if (timer != null) {
         clearTimeout(timer);
